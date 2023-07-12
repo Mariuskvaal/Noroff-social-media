902bce78db9042b27e678c79a67b06c62cafdfb7
Mariuskvaal <mariuskvaal1@gmail.com>
2023-07-12 17:20:56 +0200

    update

--- posts/posts.js
+++ posts/posts.js
@@ -8,12 +8,15 @@ async function fetchAndDisplayPosts() {
   try {
     const token = localStorage.getItem("token"); // Retrieve the token from localStorage or another source
 
-    const response = await fetch("https://nf-api.onrender.com/api/v1/social/posts", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await fetch(
+      "https://nf-api.onrender.com/api/v1/social/posts",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     const posts = await response.json();
 
@@ -40,15 +43,21 @@ function displayPosts(posts) {
   postsContainer.innerHTML = "";
 
   // Generate HTML content for posts
-  const postsHTML = posts.map((post) => {
-    return `
+  const postsHTML = posts
+    .map((post) => {
+      return `
       <div class="container.for.posts">
         <h2 class="post.title.css">${post.title}</h2>
         <p class="post.body.css">${post.body}</p>
-        ${post.media ? `<img class="post.image.css" src="${post.media}" alt="Post Image">` : ""}
+        ${
+          post.media
+            ? `<img class="post.image.css" src="${post.media}" alt="Post Image">`
+            : ""
+        }
       </div>
     `;
-  }).join("");
+    })
+    .join("");
 
   // Set the generated HTML content to the container
   postsContainer.innerHTML = postsHTML;
@@ -66,19 +75,24 @@ function filterPosts() {
 
   // If there's an ID input, only filter by ID
   if (searchId) {
-    filteredPosts = filteredPosts.filter(post => post.id && post.id === searchId);
+    filteredPosts = filteredPosts.filter(
+      (post) => post.id && post.id === searchId
+    );
   } else {
     // Apply other filters if ID filter is not used
     if (filterType === "image") {
-      filteredPosts = filteredPosts.filter(post => post.media);
+      filteredPosts = filteredPosts.filter((post) => post.media);
     } else if (filterType === "body") {
-      filteredPosts = filteredPosts.filter(post => post.body && post.body.trim().length > 0);
+      filteredPosts = filteredPosts.filter(
+        (post) => post.body && post.body.trim().length > 0
+      );
     }
 
     // Filter by search term
-    filteredPosts = filteredPosts.filter(post =>
-      (post.title && post.title.toLowerCase().includes(searchTerm)) ||
-      (post.body && post.body.toLowerCase().includes(searchTerm))
+    filteredPosts = filteredPosts.filter(
+      (post) =>
+        (post.title && post.title.toLowerCase().includes(searchTerm)) ||
+        (post.body && post.body.toLowerCase().includes(searchTerm))
     );
   }
 
@@ -91,13 +105,5 @@ filterInput.addEventListener("input", filterPosts);
 filterSelect.addEventListener("change", filterPosts);
 idInput.addEventListener("input", filterPosts);
 
-
-
 // Call the function to fetch and display posts
 fetchAndDisplayPosts();
-
-
-
-
-
-
